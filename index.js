648bea4c54b025d4f9a533efda559d6ea438a573
Bazooka J <grubcowboy@gmail.com>
2024-08-27 17:15:47 -0700

    created planetRetro vars to determine display for direct v retro motion, sortPlanet() splits array to set elements, HMTL and CSS updates to the svgs and elements

--- index.js
+++ index.js
@@ -17,24 +17,42 @@ async function main() {
         let long = location.coords.longitude;
         console.log(lat, long);
         console.log("accuracy: ", location.coords.accuracy);
+        // lat/long for birth place - testing
+        lat = 40.733550;
+        long = -74.008660;
 
+        // NOTE - months go from 0 - 11 (0 = jan)
         const ephemeris = new Ephemeris.default({
-            year: 2024,
-            month: 7,
-            day: 15,
-            hours: 16,
-            minutes: 20,
+            year: 1988,
+            month: 3,
+            day: 28,
+            hours: 8,
+            minutes: 11,
             latitude: lat,
             longitude: long,
             calculateShadows: false,
         });
+
         console.log(`${ephemeris.Observer.latitude} lat, ${ephemeris.Observer.longitude} long`);
         console.log(ephemeris.Results);
-        console.log(ephemeris.mercury.motion);
-        console.log(ephemeris.sun.motion);
-        console.log(ephemeris.moon.motion);
+        console.log('mercury retrograde: ', ephemeris.mercury.motion.isRetrograde);
 
-        setMotionBar();
+        const planetRetro = {
+            'mercury': ephemeris.mercury.motion.isRetrograde,
+            'venus': ephemeris.venus.motion.isRetrograde,
+            'mars': ephemeris.mars.motion.isRetrograde,
+            'jupiter': ephemeris.jupiter.motion.isRetrograde,
+            'saturn': ephemeris.saturn.motion.isRetrograde,
+            'neptune': ephemeris.neptune.motion.isRetrograde,
+            'uranus': ephemeris.uranus.motion.isRetrograde,
+            'pluto': ephemeris.pluto.motion.isRetrograde,
+        };
+
+        console.log(planetRetro);
+
+        sortPlanet(planetRetro);
+
+
 
     } catch (e) {
         console.log('ERROR');
@@ -44,8 +62,28 @@ async function main() {
 
 main();
 
+
+function sortPlanet(obj) {
+    const retroSet = [], directSet = [];
+    for (const [planet, isRetrograde] of Object.entries(obj)) {
+        if (isRetrograde) {
+            retroSet.push(planet);
+        } else {
+            directSet.push(planet);
+        }
+    }
+    console.log(`directSet: ${directSet}`);
+    console.log(`retroSet: ${retroSet}`);
+
+
+
+    setMotionBar();
+
+};
+
+
 function setMotionBar() {
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 8; i++) {
         const segment = document.createElement("div");
         segment.setAttribute("class", "segment");
         motionBar.appendChild(segment);
